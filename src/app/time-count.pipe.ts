44dde1bebe8d9b1258ad6bd165f824849ddde897
KevinMokoro <klosvoke1@gmail.com>
2019-01-14 10:12:14 +0300

    add time count.

---
+++ src/app/time-count.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeCount'
+})
+export class TimeCountPipe implements PipeTransform {
+
+  transform(value: any): number {
+    let today:Date = new Date();
+    var dateDifference = Math.abs(value-today)
+
+    var dateDifferenceSeconds=dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds
+    if (dateCounter >= 1 && value > today){
+        return dateCounter;
+    }else{
+        return 0;
+    }
+  }
+
+}
