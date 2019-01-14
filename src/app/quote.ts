c29928723b3736eea3937af596b77e880bcdaaf8
KevinMokoro <klosvoke1@gmail.com>
2019-01-14 12:07:26 +0300

    add changes to form.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription:boolean;
-  constructor(public id:number,public name:string, public status:string, public description:string){
+  constructor(public id:number,public name:string, public status:string, public description:string, public postDate:Date){
     this.showDescription=false
   }
 }
